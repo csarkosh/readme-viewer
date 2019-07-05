cf7660c8c3c43e0c2e00df0d2296b763f097eca8
Cyrus <csarkosh@uw.edu>
2019-07-05 16:05:14 -0700

    Fix compiler error

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppBar, Grid, IconButton, Modal, Paper, Tooltip, Typography} from '@material-ui/core';
+import {AppBar, Grid, IconButton, Tooltip, Typography} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { FaGithub } from 'react-icons/fa';
 import ResumeIcon from "./ResumeIcon";
