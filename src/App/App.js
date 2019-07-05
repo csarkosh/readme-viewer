d5eabe8ce951b765ff8b0118b594e4657944498f
Cyrus <csarkosh@uw.edu>
2019-07-05 14:45:11 -0700

    Update AppBar

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {AppBar, Grid, IconButton, Typography} from '@material-ui/core';
+import {AppBar, Grid, IconButton, Modal, Paper, Tooltip, Typography} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import { FaGithub } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa';
+import ResumeIcon from "./ResumeIcon";
 
-const styles = theme => ({
+const styles = () => ({
     appBar: {
-        height: 40,
+        height: 50,
     },
     appBarLogo: {
         margin: '8px 0 0 8px'
@@ -18,24 +19,69 @@ const styles = theme => ({
         textAlign: 'center',
         wordBreak: 'break-word'
     },
+    modalPaper: {
+        height: '65%',
+        margin: '56px auto 0',
+        maxWidth: 800,
+        padding: 24,
+    },
 });
 
-const App = ({ classes }) => (
-    <div>
-        <AppBar className={classes.appBar} position="sticky">
-            <Grid className={classes.appBarGridContainer} container justify="space-between" alignContent="center">
-                <Grid item>
-                    <Typography className={classes.appBarLogo} variant="h6">csarko.sh</Typography>
-                </Grid>
-                <Grid item>
-                    <IconButton color="inherit" aria-label="GitHub" href="https://github.com/csarkosh">
-                        <FaGithub />
-                    </IconButton>
-                </Grid>
-            </Grid>
-        </AppBar>
-        <Typography className={classes.bodyText} variant="h1">Coming Soon</Typography>
-    </div>
-)
+class App extends React.Component {
+    state = {
+        resumeOpen: false
+    }
+
+    handleResumeClose = () => this.setState({ resumeOpen: false })
+    handleResumeOpen = () => this.setState({ resumeOpen: true })
+
+    render() {
+        const { classes } = this.props
+        return (
+            <div>
+                <AppBar className={classes.appBar} position="sticky">
+                    <Grid className={classes.appBarGridContainer} container justify="space-between" alignContent="center"
+                          wrap="nowrap">
+                        <Grid item>
+                            <Typography className={classes.appBarLogo} variant="h6">csarko.sh</Typography>
+                        </Grid>
+                        <Grid container item justify="flex-end">
+                            <Grid item>
+                                <Tooltip title="My Resume">
+                                    <IconButton color="inherit" href="#" aria-label="Resume" style={{marginTop: 2}} onClick={this.handleResumeOpen}>
+                                        <ResumeIcon/>
+                                    </IconButton>
+                                </Tooltip>
+                            </Grid>
+                            <Grid item>
+                                <Tooltip title="My GitHub">
+                                    <IconButton color="inherit" aria-label="GitHub" href="https://github.com/csarkosh">
+                                        <FaGithub/>
+                                    </IconButton>
+                                </Tooltip>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </AppBar>
+                <Typography className={classes.bodyText} variant="h1">Coming Soon</Typography>
+                <Modal
+                    aria-label="My Resume Modal"
+                    onClose={this.handleResumeClose}
+                    onEscapeKeyDown={this.handleResumeClose}
+                    open={this.state.resumeOpen}
+                >
+                    <Paper className={classes.modalPaper}>
+                        <iframe
+                            title="My Resume"
+                            height="100%"
+                            width="100%"
+                            src="https://s3-us-west-2.amazonaws.com/csarko.sh/CyrusSarkosh.resume.pdf"
+                        />
+                    </Paper>
+                </Modal>
+            </div>
+        )
+    }
+}
 
 export default withStyles(styles)(App);
---
+++ src/App/ResumeIcon.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default () => (
+    <svg
+        id="Layer_1"
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        height="20px"
+        width="20px"
+        fill="white"
+        viewBox="0 0 512 512"
+    >
+        <path stroke="white" strokeWidth={21} d="M485.88,512H26.12V0h293L485.88,161.64ZM44.12,494H467.88V169.26L311.82,18H44.12Z" />
+        <polygon stroke="white" strokeWidth={21} points="477.08 174.17 306.66 174.17 306.66 8.96 324.66 8.96 324.66 156.17 477.08 156.17 477.08 174.17" />
+        <path stroke="white" strokeWidth={21} d="M170,182a68,68,0,1,1,68-68A68.08,68.08,0,0,1,170,182Zm0-118a50,50,0,1,0,50,50A50.06,50.06,0,0,0,170,64Z" />
+        <path stroke="white" strokeWidth={21} d="M257.83,230h-18V209.67a35,35,0,0,0-35-35h-5.17l-6.1-10.46,11.27-6.58v-1l1.6,0,2.67-1.56,1,1.8a53.07,53.07,0,0,1,47.68,52.73Z" />
+        <path stroke="white" strokeWidth={21} d="M104.83,230h-18V209.67a53.07,53.07,0,0,1,48.85-52.84l2.87-3.18,13.38,12-8.08,9h-4a35,35,0,0,0-35,35Z" />
+        <rect stroke="white" strokeWidth={21} x="87" y="261.5" width="338" height="18" />
+        <rect stroke="white" strokeWidth={21} x="87" y="315.83" width="338" height="18" />
+        <rect stroke="white" strokeWidth={21} x="87" y="370.17" width="338" height="18" />
+        <rect stroke="white" strokeWidth={21} x="87" y="424.5" width="176.67" height="18" />
+    </svg>
+)
