b411a76a1b68f2a4508cfa9c13963010a66356b9
Cyrus <csarkosh@uw.edu>
2019-07-06 15:56:51 -0700

    Update src dir structure

--- src/ProjectCard/ProjectCard.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ProjectCard from './ProjectCard';
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ProjectCard />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
