f8d5c2ee3921e70cac47866cd14616d439febde4
Cyrus <csarkosh@uw.edu>
2019-07-16 22:05:13 -0700

    Fix gateway-lambda perms

--- infrastructure/modules/cloudfront-gateway-s3/main.tf
+++ infrastructure/modules/cloudfront-gateway-s3/main.tf
@@ -40,12 +40,12 @@ resource "aws_api_gateway_resource" "resource" {
   parent_id = "${aws_api_gateway_rest_api.rest_api.root_resource_id}"
   path_part = "{proxy+}"
 }
-//resource "aws_api_gateway_method" "proxy" {
-//  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
-//  resource_id = "${aws_api_gateway_resource.resource.id}"
-//  authorization = "NONE"
-//  http_method = "ANY"
-//}
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_resource.resource.id}"
+  authorization = "NONE"
+  http_method = "ANY"
+}
 resource "aws_api_gateway_method" "proxy_root" {
   rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
   resource_id = "${aws_api_gateway_rest_api.rest_api.root_resource_id}"
@@ -56,22 +56,27 @@ resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
   stage_name = "${var.name}"
   depends_on = [
-    //"aws_api_gateway_integration.gateway_lambda",
-    "aws_api_gateway_integration.gateway_lambda_root"
+    "aws_api_gateway_integration.gateway_lambda_root",
+    "aws_api_gateway_integration.gateway_lambda"
   ]
+}
+resource "aws_api_gateway_base_path_mapping" "mapping" {
+  api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  domain_name = "${var.domain_name}"
+  stage_name = "${aws_api_gateway_deployment.deployment.stage_name}"
 }
 
 
 
 // Setup gateway-retired-lambda integration
-//resource "aws_api_gateway_integration" "gateway_lambda" {
-//  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
-//  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-//  http_method = "${aws_api_gateway_method.proxy.http_method}"
-//  integration_http_method = "POST"
-//  type = "AWS_PROXY"
-//  uri = "${var.lambda_invoke_arn}"
-//}
+resource "aws_api_gateway_integration" "gateway_lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
+  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy.http_method}"
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = "${var.lambda_invoke_arn}"
+}
 resource "aws_api_gateway_integration" "gateway_lambda_root" {
   rest_api_id = "${aws_api_gateway_rest_api.rest_api.id}"
   resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
@@ -81,11 +86,11 @@ resource "aws_api_gateway_integration" "gateway_lambda_root" {
   uri = "${var.lambda_invoke_arn}"
 }
 resource "aws_lambda_permission" "apigw_perm" {
-  statement_id = "AllowExecutionFromApiGateway"
+  statement_id = "AllowAPIGatewayInvoke"
   action = "lambda:InvokeFunction"
   function_name = "${var.lambda_func_name}"
   principal = "apigateway.amazonaws.com"
-  source_arn = "${aws_api_gateway_deployment.deployment.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
 }
 
 
@@ -97,11 +102,6 @@ resource "aws_api_gateway_domain_name" "domain_name" {
   endpoint_configuration {
     types = ["REGIONAL"]
   }
-}
-resource "aws_api_gateway_base_path_mapping" "base_mapping" {
-  api_id = "${aws_api_gateway_rest_api.rest_api.id}"
-  domain_name = "${aws_api_gateway_domain_name.domain_name.domain_name}"
-  stage_name = "${aws_api_gateway_deployment.deployment.stage_name}"
 }
 
 
@@ -171,12 +171,6 @@ output "invoke_url" {
 output "domain_name" {
   value = "${aws_api_gateway_domain_name.domain_name.domain_name}"
 }
-//output "regional_domain_name" {
-//  value = "${aws_api_gateway_domain_name.domain_name.regional_domain_name}"
-//}
-//output "regional_zone_id" {
-//  value = "${aws_api_gateway_domain_name.domain_name.regional_zone_id}"
-//}
 output "regional_domain_name" {
   value = "${aws_cloudfront_distribution.protocol_proxy.domain_name}"
 }
