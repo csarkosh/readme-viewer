01c75f74c7e25acad2c7e7982bcce13956cba73a
Cyrus <csarkosh@uw.edu>
2019-07-16 22:31:32 -0700

    Add security headers. Temporarily disabled content-security-policy header

--- lambdas/server/exports.js
+++ lambdas/server/exports.js
@@ -4,7 +4,11 @@ exports.handler = (event, context, callback) => {
     var response = {
         statusCode: 200,
         headers: {
-            'Content-Type': 'text/html; charset=utf-8'
+            'Content-Type': 'text/html; charset=utf-8',
+            'Strict-Transport-Security': 'max-age=15768001; includeSubDomains',
+            'X-Content-Type-Options': 'nosniff',
+            'X-Frame-Options': 'sameorigin',
+            'X-XSS-Protection': '1'
         },
         body: fs.readFileSync('./index.html').toString('utf-8')
     }
