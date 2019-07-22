95d7014347faa58118257117001db78f4908f36e
Cyrus <csarkosh@uw.edu>
2019-07-21 17:04:16 -0700

    Add lambda code to cache github data and store on s3

---
+++ lambdas/cache-gh-data/exports.js
@@ -0,0 +1,55 @@
+const aws = require('aws-sdk')
+const axios = require('axios')
+
+
+exports.handler = async () => {
+    await new Promise((resolve, reject) => {
+        (new aws.SecretsManager({ region: 'us-east-1' }))
+            .getSecretValue({ SecretId: 'gh/read_token' }, (err, data) => {
+                if (err !== null) {
+                    reject(err)
+                } else {
+                    resolve(JSON.parse(data.SecretString).gh_read_token)
+                }
+            })
+    }).then(token => axios.post(
+        'https://api.github.com/graphql',
+        {
+            query: `query {
+                user(login:"csarkosh") {
+                    repositories(first:10, ownerAffiliations:[OWNER], privacy:PUBLIC) {
+                        edges {
+                            node {
+                                description
+                                id
+                                isFork
+                                name
+                                url
+                                parent {
+                                    nameWithOwner
+                                    url
+                                }
+                            }
+                        }
+                    }
+                }
+            }`
+        },
+        { headers: { Authorization: `bearer ${token}`}
+    })).then(({ data }) => new Promise((resolve, reject) => {
+        if (data && data.data && data.data.user && data.data.user.repositories && data.data.user.repositories.edges && data.data.user.repositories.edges.length) {
+            (new aws.S3()).upload({
+                Body: JSON.stringify(data.data.user.repositories.edges.map(({ node }) => node)),
+                Bucket: 'csarko.sh',
+                Key: 'data/repos.json'
+            }, err => {
+                if (err !== null) {
+                    reject(err)
+                } else {
+                    resolve()
+                }
+            })
+        }
+    }))
+    .catch(console.error)
+}
