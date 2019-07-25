1fc4ab425b32161dfac294dd1860426cc4fb0d0c
Cyrus <csarkosh@uw.edu>
2019-07-25 15:12:29 -0700

    Increase lambda execution time

--- infrastructure/modules/scheduled-lambda/main.tf
+++ infrastructure/modules/scheduled-lambda/main.tf
@@ -62,6 +62,7 @@ resource "aws_lambda_function" "func" {
   runtime = "nodejs10.x"
   s3_bucket = "${var.s3_bucket}"
   s3_key = "${var.s3_key}"
+  timeout = 60
 }
 resource "aws_cloudwatch_event_rule" "rule" {
   name = "${var.name}-event-rule"
